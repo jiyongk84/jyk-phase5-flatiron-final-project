c38ca6a53b05d8897f5364caa684a7d55da6e331
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-10-07 23:08:00 -0400

    add crud for sign up/in/out

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session, jsonify
 from flask_restful import Resource
 
 # Local imports
@@ -13,10 +13,94 @@ from models import Airport, User, Flight, Booking, Payment
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
 
+        username = data.get('username')
+        password = data['password']
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+
+        if not username or not password or not first_name or not last_name or not email:
+            return {"error": "422 Unprocessable Entity"}, 422
+
+        user = User(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+        user.password = password  # Assuming you have a password setter method in your User model
+
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+
+        user_data = {
+            'user_id': user.id,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+        }
+
+        return jsonify(user_data), 201
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id is not None:
+            user = User.query.get(user_id)
+            if user:
+                response_data = {
+                    'user_id': user.id,
+                    'username': user.username,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'email': user.email,
+                }
+                return jsonify(response_data), 200
+        return {'error': 'Unauthorized'}, 401
+
+class Login(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            username = data.get('username')
+            password = data.get('password')
+
+            user = User.query.filter_by(username=username).first()
+            if user and (password is None or user.check_password(password)):
+                session['user_id'] = user.id
+                response_data = {
+                    'user_id': user.id,
+                    'username': user.username,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'email': user.email,
+                }
+                return jsonify(response_data), 200
+            else:
+                return {'error': 'Unauthorized'}, 401
+        except Exception as e:
+            return {'error': str(e)}, 500
+
+class Logout(Resource):
+    def delete(self):
+        if session.get('user_id'):
+            session.pop('user_id', None)
+            return {}, 204
+        else:
+            return {'error': 'Unauthorized'}, 401
+
+# Add your resource routes
+api.add_resource(Signup, '/api/users/signup')
+api.add_resource(CheckSession, '/api/users/checksession')
+api.add_resource(Login, '/api/users/login')
+api.add_resource(Logout, '/api/users/logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
