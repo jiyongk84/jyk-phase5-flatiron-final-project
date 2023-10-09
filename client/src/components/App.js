4f063e259fb1a628ca2e8b4da8f7e4778f1427c8
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-10-09 02:21:04 -0400

    add searchresults

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
+import SearchResults from './SearchResults';
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
   return (
     <Router>
       <div className="App">
@@ -12,13 +14,16 @@ function App() {
           <Link to="/profile">Profile</Link>
         </nav>
         <Switch>
-          <Route path="/" exact render={() => (
+          <Route path="/" exact component={() => (
             <div>
               <h1>Paradise Airlines</h1>
-              <Home />
+              <Home onSearch={setSearchResults} /> {/* Pass setSearchResults function */}
             </div>
           )} />
           <Route path="/profile" component={Profile} />
+          <Route path="/search-results" component={() => (
+            <SearchResults searchResults={searchResults} /> // Pass searchResults as a prop
+          )} />
         </Switch>
       </div>
     </Router>
