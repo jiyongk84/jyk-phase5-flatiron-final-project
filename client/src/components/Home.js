0b9d166e7d843fa82d78b08abd6612b822acee2f
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-10-10 16:44:28 -0400

    removed unnecessary lines

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import SearchFlight from './SearchFlight'; // Import the SearchFlight component
-import BookFlight from './BookFlight'; // Import the BookFlight component
 
 function Home() {
   return (
     <div className="home">
       <h1>Welcome to Flight Booking</h1>
 
-      {/* Render the SearchFlight component */}
       <SearchFlight />
-
-      {/* Render the BookFlight component */}
-      {/* <BookFlight /> */}
     </div>
   );
 }
