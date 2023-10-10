64b974162545b2e556ce1b5a76a100fdae4b8aad
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-10-10 16:43:13 -0400

    removed BookFlight

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import SearchFlight from './SearchFlight';
+import SearchFlight from './SearchFlight'; // Import the SearchFlight component
+import BookFlight from './BookFlight'; // Import the BookFlight component
 
 function Home() {
   return (
-    <div>
+    <div className="home">
+      <h1>Welcome to Flight Booking</h1>
+
+      {/* Render the SearchFlight component */}
       <SearchFlight />
+
+      {/* Render the BookFlight component */}
+      {/* <BookFlight /> */}
     </div>
   );
 }
